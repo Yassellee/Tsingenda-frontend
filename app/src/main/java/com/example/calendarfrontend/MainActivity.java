23d353cc7b2585d071b6b9fb44eea847931e9ea2
金雨琳 <2019011229@secoder.net>
2022-12-09 23:11:43 +0800

    fix picture with backend

--- app/src/main/java/com/example/calendarfrontend/MainActivity.java
+++ app/src/main/java/com/example/calendarfrontend/MainActivity.java
@@ -14,11 +14,20 @@ import android.os.FileObserver;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
@@ -30,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = "Screenshot";
     private ImageView shot;
     private CustomFileObserver mFileObserver;
+    Map<String, String> photomap = new HashMap<String, String>();
 
     private static final String PATH = Environment.getExternalStorageDirectory() + File.separator
             + Environment.DIRECTORY_PICTURES + File.separator + "Screenshots" + File.separator;
@@ -75,56 +85,85 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onEvent(int event, String path) {
             if (path != null){
-                UpLoadPicture(PATH + path);
+                try {
+                    UpLoadPicture(PATH + path);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-    private void UpLoadPicture(String path) {
+    private void UpLoadPicture(String path) throws JSONException {
         //判断文件夹中是否有文件存在
         File file = new File(path);
         if (!file.exists()){
-            System.out.println("nonono");
             runOnUiThread(() -> Toast.makeText(MainActivity.this, "图片不存在", Toast.LENGTH_SHORT).show());
         }
         else {
-            System.out.println(path);
             runOnUiThread(() -> shot.setImageURI(Uri.fromFile(file)));
+            if(photomap.containsKey(path)){
+                System.out.println("return directly");
+                return;
+            }
+            else {
+                photomap.put(path,"1");
+            }
         }
-//        OkHttpClient client = new OkHttpClient();
-//        String imgPath = path;
-//        MultipartBody.Builder builder = new MultipartBody.Builder();
-//        builder.addFormDataPart("file",imgPath, RequestBody.create(MediaType.parse("png"),new File(imgPath)));
-//        RequestBody requestBody = builder.build();
-//        Request.Builder reqBuilder = new Request.Builder();
-//        Request request = reqBuilder
-//                .url(R.string.url+"/tsingenda/login/")
-//                .post(requestBody)
-//                .build();
-//        Call call = client.newCall(request);
-//        call.enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        //  aBuilder.dismiss();
-//                        Toast.makeText(getApplicationContext(),"上传失败",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                final String resp = response.body().toString();
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        //  aBuilder.dismiss();
-//                        Toast.makeText(getApplicationContext(),resp,Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
+        MediaType JSON = MediaType.parse("application/json;charset=utf-8");
+        JSONObject photo = new JSONObject();
+        try {
+            photo.put("data",imageToBase64Str(path));
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        OkHttpClient client = new OkHttpClient();
+        FormBody.Builder builder = new FormBody.Builder();
+        Request.Builder reqBuilder = new Request.Builder();
+        RequestBody requestBody = RequestBody.create(JSON, String.valueOf(photo));
+        Request request = reqBuilder
+                .url(getString(R.string.url)+"/tsingenda/image/")
+                .post(requestBody)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //  aBuilder.dismiss();
+                        Toast.makeText(getApplicationContext(),"上传失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                System.out.println(response.toString());
+                //TODO:add response to user
+            }
+        });
+    }
+
+    public static String imageToBase64Str(String imgFile) {
+        InputStream inputStream = null;
+        byte[] data = null;
+        try {
+            inputStream = new FileInputStream(imgFile);
+            data = new byte[inputStream.available()];
+            inputStream.read(data);
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 加密
+        Base64.Encoder encoder = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            encoder = Base64.getEncoder();
+            return encoder.encodeToString(data);
+        }
+        return imgFile;
     }
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
